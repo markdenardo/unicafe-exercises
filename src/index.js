59c22caa43f66754d3b4299c455e4a8f1c07f882
markdenardo <mark.denardo@gmail.com>
2020-11-30 16:17:48 -0500

    solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
@@ -6,21 +6,44 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [sum, setSum] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const calculateAverage = () => {
+
+  }
 
   const goodValue = () => {
     console.log('clicked')
     setGood(good+1)
-  }
+    setAll(all+1)   
+    setSum(sum+1)
 
+  }
   const neutralValue = () => {
     console.log('clicked')
     setNeutral(neutral+1)
+    setAll(all + 1)
+    setSum(sum+0)
   }
 
   const badValue = () => {
     console.log('clicked')
     setBad(bad+1)
+    setAll(all + 1)
+    setSum(sum-1)
   }
+
+  const setStats = () => {
+      console.log(all)
+      setAverage(sum / all)
+      setPositive(sum / good * 100)
+
+  }
+
+  useEffect(setStats, [all, good, sum])
 
   return (
     <div>
@@ -34,6 +57,10 @@ const App = () => {
       <ul>good {good}</ul>
       <ul>neutral {neutral}</ul>
       <ul>bad {bad}</ul>
+      <ul>all {all}</ul>
+      <ul>sum {sum}</ul>
+      <ul>average {average}</ul>
+      <ul>positive {positive}</ul>
     </div>
   )
 }
