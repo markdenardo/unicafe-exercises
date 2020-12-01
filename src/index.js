af70a9bcd68eb6d1f34f1b107a97e9e5f2f72c01
markdenardo <mark.denardo@gmail.com>
2020-11-30 20:33:57 -0500

    1.13 progress

--- src/index.js
+++ src/index.js
@@ -65,36 +65,53 @@ import ReactDOM from 'react-dom'
 const App = (props) => {
   
   const [selected, setSelected] = useState(0)
+  const [array, setArray] = useState([])
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
   const random = () => {
-   let index = props.anecdotes.length
+    getArray()
+   let index = array.length
       if (index !== undefined){
         setSelected(getRandomInt(index))
-        console.log(selected)
+        // console.log(selected)
       }
   }
 
-  useEffect (random, []) 
+  const getArray = () => {
+    // debugger
+    // console.log(props)
+    setArray(props.anecdotes)
+  //  console.log(array)
+  }
+
+  const vote = () => {
+
+  }
+
+  useEffect(random, array) 
 
   return (
     <div>
-      {props.anecdotes[selected]}<br/>
+      <br/>
+      {props.anecdotes[selected].quote}<br/>
+      This quote has {props.anecdotes[selected].votes} votes<br/>
+      {/* This quote has {array[selected].votes} votes <br/> */}
       <button onClick={()=>random()}>new anecdote</button>
+      <button onClick={()=>vote()}>vote</button>
     </div>
   )
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  {quote: 'If it hurts, do it more often', votes: 0},
+  {quote:'Adding manpower to a late software project makes it later!', votes: 0},
+  {quote: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0},
+  {quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0},
+  {quote: 'Premature optimization is the root of all evil.', votes: 0},
+  {quote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes: 0}
 ]
 
 
